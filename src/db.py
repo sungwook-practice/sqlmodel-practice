710475f1ee77a6072a78ad4f6eafa80df57136e3
choisungwook <kgg1959@naver.com>
2022-10-15 23:00:54 +0900

    hero insert 함수 추가

--- src/db.py
+++ src/db.py
@@ -1,11 +1,23 @@
 from typing import Optional
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine
 
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     secret_name: str
     age: Optional[int] = None
+
+def create_heroes():
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+    with Session(engine) as session:
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+
+        session.commit()
 
 sqlite_file_name = "sample.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -16,3 +28,4 @@ def create_db_and_tables():  #
 
 if __name__ == "__main__":  #
     create_db_and_tables()
+    create_heroes()
