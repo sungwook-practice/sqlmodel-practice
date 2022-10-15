c90f1dae83329dc51e9fed4bf02b14110fe2606f
choisungwook <kgg1959@naver.com>
2022-10-15 23:12:30 +0900

    refresh 예제 추가

--- src/db.py
+++ src/db.py
@@ -12,12 +12,28 @@ def create_heroes():
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
 
+    print("Before committing  the session")
+    print(hero_1)
+    print(hero_2)
+    print(hero_3)
+
     with Session(engine) as session:
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
 
         session.commit()
+
+        print("After committing the session")
+        session.refresh(hero_1)
+        print("Hero 1:", hero_1)
+        print("Hero 2:", hero_2)
+        print("Hero 3:", hero_3)
+
+        print("After committing the session, show names")
+        print("Hero 1 name:", hero_1.name)
+        print("Hero 2 name:", hero_2.name)
+        print("Hero 3 name:", hero_3.name)
 
 sqlite_file_name = "sample.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
