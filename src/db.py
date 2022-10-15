509f9c6dbcc5a6023133b3059d10a9a1518428d0
choisungwook <kgg1959@naver.com>
2022-10-16 01:13:39 +0900

    조인 예제 추가

--- src/db.py
+++ src/db.py
@@ -7,43 +7,51 @@ class Hero(SQLModel, table=True):
     secret_name: str
     age: Optional[int] = None
 
+    team_id: Optional[int] = Field(default=None, foreign_key='team.id')
+
+class Team(SQLModel, table=True):
+    id: Optional[int] =  Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
+
 def create_heroes():
-    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
-    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
-    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
-
-    print("Before committing  the session")
-    print(hero_1)
-    print(hero_2)
-    print(hero_3)
-
     with Session(engine) as session:
-        session.add(hero_1)
-        session.add(hero_2)
-        session.add(hero_3)
-
+        team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
+        team_z_force = Team(name="Z-Force", headquarters="Sister Margaret’s Bar")
+        session.add(team_preventers)
+        session.add(team_z_force)
         session.commit()
 
-        print("After committing the session")
-        session.refresh(hero_1)
-        print("Hero 1:", hero_1)
-        print("Hero 2:", hero_2)
-        print("Hero 3:", hero_3)
+        hero_deadpond = Hero(
+            name="Deadpond", secret_name="Dive Wilson", team_id=team_z_force.id
+        )
+        hero_rusty_man = Hero(
+            name="Rusty-Man",
+            secret_name="Tommy Sharp",
+            age=48,
+            team_id=team_preventers.id,
+        )
+        hero_spider_boy = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
 
-        print("After committing the session, show names")
-        print("Hero 1 name:", hero_1.name)
-        print("Hero 2 name:", hero_2.name)
-        print("Hero 3 name:", hero_3.name)
+        session.refresh(hero_deadpond)
+        session.refresh(hero_rusty_man)
+        session.refresh(hero_spider_boy)
+
+        print("Created hero:", hero_deadpond)
+        print("Created hero:", hero_rusty_man)
+        print("Created hero:", hero_spider_boy)
+
 
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero)
+        statement = select(Hero, Team).join(Team, isouter=True)
         results = session.exec(statement)
-        # heroes = results.all()
-        # print(heroes)
-
-        for hero in results:
-            print(hero)
+        for hero, team in results:
+            print("Hero:", hero, "Team:", team)
 
 
 sqlite_file_name = "sample.db"
@@ -54,6 +62,6 @@ def create_db_and_tables():  #
     SQLModel.metadata.create_all(engine)  #
 
 if __name__ == "__main__":  #
-    create_db_and_tables()
+    # create_db_and_tables()
     # create_heroes()
     select_heroes()
