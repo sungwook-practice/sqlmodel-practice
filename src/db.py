5738330780f4a8ea021d65de196c089b7933305e
choisungwook <kgg1959@naver.com>
2022-10-15 22:53:55 +0900

    리팩토링: 원할떄만 db 업데이트

--- src/db.py
+++ src/db.py
@@ -11,6 +11,8 @@ sqlite_file_name = "sample.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 engine = create_engine(sqlite_url, echo=True)
 
-SQLModel.metadata.create_all(engine)
+def create_db_and_tables():  #
+    SQLModel.metadata.create_all(engine)  #
 
-print("done")
+if __name__ == "__main__":  #
+    create_db_and_tables()
